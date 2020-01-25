e018b5a10ffea48b659db85c2e125487f270c05a
spiffytech <spiffy@spiffy.tech>
2020-01-25 10:08:07 -0500

    Rewrite with modern library and UI framework. Added support for choosing randomization engine. Added support for persisting selected translation.

---
+++ data.js
@@ -0,0 +1,436 @@
+window.translations = [
+  {
+    id: 1588,
+    local_title: "Amplified Bible",
+    audio: false,
+    local_abbreviation: "AMP"
+  },
+  {
+    id: 8,
+    local_title: "Amplified Bible, Classic Edition",
+    audio: false,
+    local_abbreviation: "AMPC"
+  },
+  {
+    id: 12,
+    local_title: "American Standard Version",
+    audio: true,
+    local_abbreviation: "ASV"
+  },
+  {
+    id: 31,
+    local_title: "The Books of the Bible NT",
+    audio: false,
+    local_abbreviation: "BOOKS"
+  },
+  {
+    id: 37,
+    local_title: "Common English Bible",
+    audio: false,
+    local_abbreviation: "CEB"
+  },
+  {
+    id: 392,
+    local_title: "Contemporary English Version",
+    audio: false,
+    local_abbreviation: "CEV"
+  },
+  {
+    id: 303,
+    local_title: "Contemporary English Version Interconfessional Edition",
+    audio: false,
+    local_abbreviation: "CEVDCI"
+  },
+  {
+    id: 294,
+    local_title: "Contemporary English Version (Anglicised) 2012",
+    audio: false,
+    local_abbreviation: "CEVUK"
+  },
+  {
+    id: 1275,
+    local_title: "Complete Jewish Bible",
+    audio: false,
+    local_abbreviation: "CJB"
+  },
+  {
+    id: 42,
+    local_title: "Catholic Public Domain Version",
+    audio: false,
+    local_abbreviation: "CPDV"
+  },
+  {
+    id: 1713,
+    local_title: "Christian Standard Bible",
+    audio: true,
+    local_abbreviation: "CSB"
+  },
+  {
+    id: 478,
+    local_title: "Darby's Translation 1890",
+    audio: false,
+    local_abbreviation: "DARBY"
+  },
+  {
+    id: 55,
+    local_title: "Douay-Rheims Challoner Revision 1752",
+    audio: true,
+    local_abbreviation: "DRC1752"
+  },
+  {
+    id: 2079,
+    local_title: "EasyEnglish Bible 2018",
+    audio: false,
+    local_abbreviation: "EASY"
+  },
+  {
+    id: 406,
+    local_title: "Holy Bible: Easy-to-Read Version",
+    audio: false,
+    local_abbreviation: "ERV"
+  },
+  {
+    id: 59,
+    local_title: "English Standard Version",
+    audio: true,
+    local_abbreviation: "ESV"
+  },
+  {
+    id: 1932,
+    local_title: "Free Bible Version New Testament with Psalms",
+    audio: false,
+    local_abbreviation: "FBVNTPSALMS"
+  },
+  {
+    id: 296,
+    local_title: "Good News Bible",
+    audio: false,
+    local_abbreviation: "GNB"
+  },
+  {
+    id: 416,
+    local_title: "Good News Bible (Anglicised)",
+    audio: false,
+    local_abbreviation: "GNBDC"
+  },
+  {
+    id: 431,
+    local_title: "Good News Bible (Catholic edition in Septuagint order)",
+    audio: false,
+    local_abbreviation: "GNBDK"
+  },
+  {
+    id: 68,
+    local_title: "Good News Translation",
+    audio: true,
+    local_abbreviation: "GNT"
+  },
+  {
+    id: 69,
+    local_title: "Good News Translation (US Version)",
+    audio: true,
+    local_abbreviation: "GNTD"
+  },
+  {
+    id: 2163,
+    local_title: "Geneva Bible",
+    audio: false,
+    local_abbreviation: "GNV"
+  },
+  {
+    id: 70,
+    local_title: "GOD'S WORD Translation",
+    audio: false,
+    local_abbreviation: "GW"
+  },
+  {
+    id: 1047,
+    local_title: "St Paul from the Trenches 1916",
+    audio: false,
+    local_abbreviation: "GWC"
+  },
+  {
+    id: 72,
+    local_title: "Holman Christian Standard Bible",
+    audio: true,
+    local_abbreviation: "HCSB"
+  },
+  {
+    id: 1359,
+    local_title: "International Children’s Bible",
+    audio: false,
+    local_abbreviation: "ICB"
+  },
+  {
+    id: 1077,
+    local_title: "Jubilee Bible",
+    audio: false,
+    local_abbreviation: "JUB"
+  },
+  {
+    id: 1,
+    local_title: "King James Version",
+    audio: true,
+    local_abbreviation: "KJV"
+  },
+  {
+    id: 546,
+    local_title: "King James Version with Apocrypha, American Edition",
+    audio: false,
+    local_abbreviation: "KJVA"
+  },
+  {
+    id: 547,
+    local_title: "King James Version, American Edition",
+    audio: false,
+    local_abbreviation: "KJVA"
+  },
+  {
+    id: 90,
+    local_title: "Lexham English Bible",
+    audio: true,
+    local_abbreviation: "LEB"
+  },
+  {
+    id: 1171,
+    local_title: "Modern English Version",
+    audio: false,
+    local_abbreviation: "MEV"
+  },
+  {
+    id: 1365,
+    local_title: "Metrical Psalms 1650",
+    audio: false,
+    local_abbreviation: "MP1650"
+  },
+  {
+    id: 97,
+    local_title: "The Message",
+    audio: true,
+    local_abbreviation: "MSG"
+  },
+  {
+    id: 463,
+    local_title: "New American Bible, revised edition",
+    audio: false,
+    local_abbreviation: "NABRE"
+  },
+  {
+    id: 100,
+    local_title: "New American Standard Bible",
+    audio: true,
+    local_abbreviation: "NASB"
+  },
+  {
+    id: 105,
+    local_title: "New Century Version",
+    audio: false,
+    local_abbreviation: "NCV"
+  },
+  {
+    id: 107,
+    local_title: "New English Translation",
+    audio: false,
+    local_abbreviation: "NET"
+  },
+  {
+    id: 110,
+    local_title: "New International Reader’s Version",
+    audio: false,
+    local_abbreviation: "NIRV"
+  },
+  {
+    id: 111,
+    local_title: "New International Version",
+    audio: true,
+    local_abbreviation: "NIV"
+  },
+  {
+    id: 113,
+    local_title: "New International Version (Anglicised)",
+    audio: true,
+    local_abbreviation: "NIVUK"
+  },
+  {
+    id: 114,
+    local_title: "New King James Version",
+    audio: false,
+    local_abbreviation: "NKJV"
+  },
+  {
+    id: 116,
+    local_title: "New Living Translation",
+    audio: true,
+    local_abbreviation: "NLT"
+  },
+  {
+    id: 2135,
+    local_title: "New Messianic Version Bible",
+    audio: false,
+    local_abbreviation: "NMV"
+  },
+  {
+    id: 2016,
+    local_title: "New Revised Standard Version",
+    audio: false,
+    local_abbreviation: "NRSV"
+  },
+  {
+    id: 2015,
+    local_title: "New Revised Standard Version Catholic Interconfessional",
+    audio: false,
+    local_abbreviation: "NRSV-CI"
+  },
+  {
+    id: 130,
+    local_title: "Orthodox Jewish Bible",
+    audio: false,
+    local_abbreviation: "OJB"
+  },
+  {
+    id: 2020,
+    local_title: "Revised Standard Version",
+    audio: false,
+    local_abbreviation: "RSV"
+  },
+  {
+    id: 2017,
+    local_title: "Revised Standard Version",
+    audio: false,
+    local_abbreviation: "RSV-CI"
+  },
+  {
+    id: 477,
+    local_title: "Revised Version 1885",
+    audio: false,
+    local_abbreviation: "RV1885"
+  },
+  {
+    id: 1922,
+    local_title: "Revised Version with Apocrypha 1895",
+    audio: false,
+    local_abbreviation: "RV1895"
+  },
+  {
+    id: 314,
+    local_title: "Tree of Life Version",
+    audio: false,
+    local_abbreviation: "TLV"
+  },
+  {
+    id: 1849,
+    local_title: "The Passion Translation",
+    audio: false,
+    local_abbreviation: "TPT"
+  },
+  {
+    id: 316,
+    local_title: "The Scriptures 2009",
+    audio: false,
+    local_abbreviation: "TS2009"
+  },
+  {
+    id: 2407,
+    local_title: "Wycliffe's Bible with Modern Spelling",
+    audio: false,
+    local_abbreviation: "WBMS"
+  },
+  {
+    id: 206,
+    local_title: "World English Bible",
+    audio: true,
+    local_abbreviation: "WEB"
+  },
+  {
+    id: 1204,
+    local_title: "World English Bible British Edition",
+    audio: false,
+    local_abbreviation: "WEBBE"
+  },
+  {
+    id: 1209,
+    local_title: "World Messianic Bible",
+    audio: false,
+    local_abbreviation: "WMB"
+  },
+  {
+    id: 1207,
+    local_title: "World Messianic Bible British Edition",
+    audio: false,
+    local_abbreviation: "WMBBE"
+  },
+  {
+    id: 821,
+    local_title: "Young's Literal Translation 3rd Revision 1898",
+    audio: false,
+    local_abbreviation: "YLT1898"
+  }
+];
+window.books = [
+  { human: "Genesis", usfm: "GEN", chapters: 50 },
+  { human: "Exodus", usfm: "EXO", chapters: 40 },
+  { human: "Leviticus", usfm: "LEV", chapters: 27 },
+  { human: "Numbers", usfm: "NUM", chapters: 36 },
+  { human: "Deuteronomy", usfm: "DEU", chapters: 34 },
+  { human: "Joshua", usfm: "JOS", chapters: 24 },
+  { human: "Judges", usfm: "JDG", chapters: 21 },
+  { human: "Ruth", usfm: "RUT", chapters: 4 },
+  { human: "1 Samuel", usfm: "1SA", chapters: 31 },
+  { human: "2 Samuel", usfm: "2SA", chapters: 24 },
+  { human: "1 Kings", usfm: "1KI", chapters: 22 },
+  { human: "2 Kings", usfm: "2KI", chapters: 25 },
+  { human: "1 Chronicles", usfm: "1CH", chapters: 29 },
+  { human: "2 Chronicles", usfm: "2CH", chapters: 36 },
+  { human: "Ezra", usfm: "EZR", chapters: 10 },
+  { human: "Nehemiah", usfm: "NEH", chapters: 13 },
+  { human: "Esther", usfm: "EST", chapters: 10 },
+  { human: "Job", usfm: "JOB", chapters: 42 },
+  { human: "Psalms", usfm: "PSA", chapters: 150 },
+  { human: "Proverbs", usfm: "PRO", chapters: 31 },
+  { human: "Ecclesiastes", usfm: "ECC", chapters: 12 },
+  { human: "Song of Solomon", usfm: "SNG", chapters: 8 },
+  { human: "Isaiah", usfm: "ISA", chapters: 66 },
+  { human: "Jeremiah", usfm: "JER", chapters: 52 },
+  { human: "Lamentations", usfm: "LAM", chapters: 5 },
+  { human: "Ezekiel", usfm: "EZK", chapters: 48 },
+  { human: "Daniel", usfm: "DAN", chapters: 12 },
+  { human: "Hosea", usfm: "HOS", chapters: 14 },
+  { human: "Joel", usfm: "JOL", chapters: 3 },
+  { human: "Amos", usfm: "AMO", chapters: 9 },
+  { human: "Obadiah", usfm: "OBA", chapters: 1 },
+  { human: "Jonah", usfm: "JON", chapters: 4 },
+  { human: "Micah", usfm: "MIC", chapters: 7 },
+  { human: "Nahum", usfm: "NAM", chapters: 3 },
+  { human: "Habakkuk", usfm: "HAB", chapters: 3 },
+  { human: "Zephaniah", usfm: "ZEP", chapters: 3 },
+  { human: "Haggai", usfm: "HAG", chapters: 2 },
+  { human: "Zechariah", usfm: "ZEC", chapters: 14 },
+  { human: "Malachi", usfm: "MAL", chapters: 4 },
+  { human: "Matthew", usfm: "MAT", chapters: 28 },
+  { human: "Mark", usfm: "MRK", chapters: 16 },
+  { human: "Luke", usfm: "LUK", chapters: 24 },
+  { human: "John", usfm: "JHN", chapters: 21 },
+  { human: "Acts", usfm: "ACT", chapters: 28 },
+  { human: "Romans", usfm: "ROM", chapters: 16 },
+  { human: "1 Corinthians", usfm: "1CO", chapters: 16 },
+  { human: "2 Corinthians", usfm: "2CO", chapters: 13 },
+  { human: "Galatians", usfm: "GAL", chapters: 6 },
+  { human: "Ephesians", usfm: "EPH", chapters: 6 },
+  { human: "Philippians", usfm: "PHP", chapters: 4 },
+  { human: "Colossians", usfm: "COL", chapters: 4 },
+  { human: "1 Thessalonians", usfm: "1TH", chapters: 5 },
+  { human: "2 Thessalonians", usfm: "2TH", chapters: 3 },
+  { human: "1 Timothy", usfm: "1TI", chapters: 6 },
+  { human: "2 Timothy", usfm: "2TI", chapters: 4 },
+  { human: "Titus", usfm: "TIT", chapters: 3 },
+  { human: "Philemon", usfm: "PHM", chapters: 1 },
+  { human: "Hebrews", usfm: "HEB", chapters: 13 },
+  { human: "James", usfm: "JAS", chapters: 5 },
+  { human: "1 Peter", usfm: "1PE", chapters: 5 },
+  { human: "2 Peter", usfm: "2PE", chapters: 3 },
+  { human: "1 John", usfm: "1JN", chapters: 5 },
+  { human: "2 John", usfm: "2JN", chapters: 1 },
+  { human: "3 John", usfm: "3JN", chapters: 1 },
+  { human: "Jude", usfm: "JUD", chapters: 1 },
+  { human: "Revelation", usfm: "REV", chapters: 22 }
+];
