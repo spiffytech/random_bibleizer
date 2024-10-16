f4105c6c9b6099e0ce2a6e076ac8b7258bbb5d0a
spiffytech <spiffy@spiffy.tech>
2024-10-16 11:37:42 -0400

    Fixed randomization algorithm for chapters-only selection. Added UI buttons for random algo selection, though they're not connected to localstorage.

--- v2/src/components/Spinner.vue
+++ v2/src/components/Spinner.vue
@@ -34,20 +34,26 @@ const localstorageTrySet = (key: string, value: string) => {
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
-const randomNumberInclExcl = (min: number, max: number) => {
-  return Math.random() * (max - min) + min;
+const randomNumberInclIncl = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+console.log(allBooks.length);
 const randomizeByBook = (selectedBooks: Book[]): Pick<Passage, 'book' | 'chapter'> => {
-  var book_index = Math.floor(Math.random() * selectedBooks.length);
-  var chapter = Math.ceil(Math.random() * selectedBooks[book_index].chapters);
+  const bookIndex = randomNumberInclIncl(0, selectedBooks.length - 1);
+  const chapter = randomNumberInclIncl(0, selectedBooks[bookIndex].chapters);
 
-  return { book: selectedBooks[book_index], chapter };
+  if ((bookIndex === 65 && chapter === 21) || (bookIndex === 0 && chapter === 0)) {
+    console.log('selecting', bookIndex, chapter);
+  }
+  return { book: selectedBooks[bookIndex], chapter };
 };
 
 const randomizeByAllChapters = (selectedBooks: Book[]): Pick<Passage, 'book' | 'chapter'> => {
   const totalChapters = selectedBooks.reduce((acc, book) => acc + book.chapters, 0);
-  const targetChapter = randomNumberInclExcl(0, totalChapters + 1);
+  const targetChapter = randomNumberInclIncl(0, totalChapters);
   let acc = 0;
   for (const book of selectedBooks) {
     for (let i = 0; i < book.chapters; i++) {
@@ -59,10 +65,10 @@ const randomizeByAllChapters = (selectedBooks: Book[]): Pick<Passage, 'book' | '
         return { book, chapter: i + 1 };
       }
 
-      acc += i;
+      acc += 1;
     }
   }
-  throw new Error('Did not find the target chapter');
+  throw new Error(`Did not find the target chapter: ${targetChapter}`);
 };
 
 const localStorageValue = <T,>(key: string, defaultValue: T): Ref<T> => {
@@ -100,24 +106,25 @@ const weightBooksEvenly = localStorageValue('weightBooksEvenly', true);
 const randomizeFn = computed(() =>
   weightBooksEvenly.value ? randomizeByBook : randomizeByAllChapters
 );
-const lsOpenAuto = localStorageValue('openInBibleAppAuotmatically', true);
-const selectedTranslation = localStorageValue('savedTranslation', 'GW');
-interface Subset {
+const autoOpenBibleApp = localStorageValue('openInBibleAppAuotmatically', true);
+
+interface BooksRange {
   name: string;
   range: [number, number];
 }
-const subsets: Subset[] = [
+const subsets: BooksRange[] = [
   { name: 'Whole Bible', range: [0, 66] },
   { name: 'Old Testament', range: [0, 39] },
   { name: 'New Testament', range: [39, 66] },
   { name: 'Psalms and Proverbs', range: [18, 20] }
 ];
-const selectedSubset = ref(subsets[0]) as Ref<Subset>;
+const selectedSubset = ref(subsets[0]) as Ref<BooksRange>;
 
+const savedTranslationAbbr = localStorageValue('savedTranslation', 'GW').value;
 const displayedPassage = ref({
   book: allBooks[0],
   chapter: 1,
-  translation: translations.find((t) => t.abbreviation === 'GW')!
+  translation: translations.find((t) => t.abbreviation === savedTranslationAbbr)!
 }) as Ref<Passage>;
 const redraw = () => {
   if (paused.value === true) {
@@ -154,6 +161,7 @@ watchEffect(() => {
   if (translation) {
     displayedPassage.value.translation = translation;
     translationDatalistValid.value = true;
+    localstorageTrySet('savedTranslation', translation.abbreviation);
   } else {
     translationDatalistValid.value = false;
   }
@@ -169,6 +177,16 @@ let vals = ref(['alpha', 'bravo', 'charlie', 'delta']);
   </sl-button>
 
   <form>
+    <p>{{ weightBooksEvenly }}</p>
+    <sl-radio-group
+      label="Randomization method"
+      :value="weightBooksEvenly"
+      @sl-change="weightBooksEvenly = $event.target.value"
+    >
+      <sl-radio :value="true"> Yes, random books </sl-radio>
+      <sl-radio :value="false"> No, random chapters </sl-radio>
+    </sl-radio-group>
+
     <sl-radio-group
       label="What to randomize"
       :value="selectedSubset.name"
