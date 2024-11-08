0ea3a2c25f9ca4929936788019edb03becde7271
spiffytech <spiffy@spiffy.tech>
2024-11-08 11:48:10 -0500

    Tweak the spacing in the header title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView, useRoute } from 'vue-router';
+import { RouterLink, RouterView, useRoute } from "vue-router";
 
 const isRouteMatch = (route: string) => useRoute().name === route;
 </script>
@@ -9,7 +9,7 @@ const isRouteMatch = (route: string) => useRoute().name === route;
     <ul class="flex -mx-4 px-4 py-4 gap-x-4 mb-8 shadow-lg">
       <li class="mr-auto text-xl font-bold jost">
         <RouterLink :to="{ name: 'home' }">Random Bibleizer</RouterLink>
-        <span id="title-suffix" class="text-gray-600 italic"></span>
+        <span id="title-suffix" class="ml-2 text-gray-600 italic"></span>
       </li>
       <li v-if="!isRouteMatch('home')">
         <RouterLink :to="{ name: 'home' }">Home</RouterLink>
