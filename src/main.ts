95f6f4a0067bfad0b9c3bed7ce721c467174b237
spiffytech <spiffy@spiffy.tech>
2024-11-11 09:44:00 -0500

    Add Honeybadger to track errors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
-import './assets/main.css';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import PrimeVue from 'primevue/config';
+import PrimeVue from "primevue/config";
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+import Honeybadger from "@honeybadger-io/js";
+import HoneybadgerVue from "@honeybadger-io/vue";
 
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App)
-  .use(createPinia())
-  .use(router)
-  .use(PrimeVue, {
-    theme: 'none'
-  })
-  .mount('#app');
+import "./assets/main.css";
+
+const app = createApp(App).use(createPinia()).use(router).use(PrimeVue, {
+  theme: "none",
+});
+
+if (import.meta.env.VITE_honeybadgerKey) {
+  const config = {
+    apiKey: import.meta.env.VITE_honeybadgerKey,
+    environment: "production",
+  };
+  app.use(HoneybadgerVue, config);
+}
+
+app.mount("#app");
